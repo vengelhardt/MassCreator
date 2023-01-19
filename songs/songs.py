7ca2c0aad9d0041271153d3cc61813ec343935c8
Victor Engelhardt <victor.engelhardt@gmail.com>
2023-01-19 16:00:01 +0100

    fix: :ambulance: use another package for db

--- songs/songs.py
+++ songs/songs.py
@@ -1,13 +1,13 @@
 import string
 
-import mariadb
+import mysql.connector
 
 import streamlit as st
 
 
 class CDatabaseAPI:
     def __init__(self):
-        self.connection = mariadb.connect(**st.secrets.songsdb)
+        self.connection = mysql.connector.connect(**st.secrets.songsdb)
         self.cursor = self.connection.cursor()
 
     def close(self):
