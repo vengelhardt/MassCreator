147ef21a675d65fabe89035475eda4a5d434db83
Victor Engelhardt <victor.engelhardt@gmail.com>
2023-01-23 20:37:48 +0100

    perf: :zap: use cache

--- ordinaire/ordinaire.py
+++ ordinaire/ordinaire.py
@@ -12,45 +12,55 @@ class COrdinaire:
     def close(self):
         self.connection.close()
 
+    @st.cache
     def search_in_database(self, title: string, type: string) -> string:
         sql_query = "SELECT text from {} WHERE title = '{}';".format(type, title)
         self.cursor.execute(sql_query)
         return self.cursor.fetchall()[0][0]
 
+    @st.cache
     def get_list(self, type: string) -> string:
         sql_query = "SELECT title from {} ;".format(type)
         self.cursor.execute(sql_query)
         kyrie_list = self.cursor.fetchall()
         return [title[0] for title in kyrie_list]
 
+    @st.cache
     def get_kyrie_list(self):
         kyrie_list = self.get_list("kyrie")
         return kyrie_list
 
+    @st.cache
     def get_kyrie_text(self, kyrie_title: string) -> string:
         kyrie_text = self.search_in_database(kyrie_title, "kyrie")
         return kyrie_text
 
+    @st.cache
     def get_sanctus_list(self):
         sanctus_list = self.get_list("sanctus")
         return sanctus_list
 
+    @st.cache
     def get_sanctus_text(self, sanctus_title: string) -> string:
         sanctus_text = self.search_in_database(sanctus_title, "sanctus")
         return sanctus_text
 
+    @st.cache
     def get_agnus_list(self):
         agnus_list = self.get_list("agnus")
         return agnus_list
 
+    @st.cache
     def get_agnus_text(self, agnus_title: string) -> string:
         agnus_text = self.search_in_database(agnus_title, "agnus")
         return agnus_text
 
+    @st.cache
     def get_gloria_list(self):
         gloria_list = self.get_list("gloria")
         return gloria_list
 
+    @st.cache
     def get_gloria_text(self, gloria_title: string) -> string:
         gloria_text = self.search_in_database(gloria_title, "gloria")
         return gloria_text
